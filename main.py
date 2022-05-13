840f4e9991c2a9b025bee9d9395991ccd84379f2
natigalsan <natividadgalsan@gmail.com>
2022-05-13 18:10:35 +0000

    explicación python

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ persona = {
 }
 print(persona["leg fav"])
 
-# Sentencia condicional:
+# # Sentencia condicional:------------------------------------------------
 
 if (edad >17):
     print ("hola")
@@ -21,4 +21,53 @@ elif(edad==15):
     print("adios, no puedes beber así que para casita!!!") 
 else: 
     print("no deberías pensar en eso")
+
+# #Sentencias cíclicas: ---------------------------------------------------
+
+contador = 0
+while (contador <11):
+    print(contador)
+    contador+=1
+
+#------------------trabajar con FOR: -------------------------------------------
+
+poema = "las rosas son rojas"
+for char in poema:
+    print(char)
+
+
+for uno in range (5):
+    print(uno)
+
+for cuatro in range (0, 10, 2): #comienzo en 0 quiero llegar hasta el 10 y que avance de 2 en 2:
+    print(cuatro) #--> for (let i =0)
+
+for cinco in range (18,3,-3):
+    print(tres) #for (let i=18, i>3, i-=3)// si 18 es > que 3 sigue restando (-3)
+#----------------------ARRAYS------------------------------------------------------------------------------
+personas = ["ore", "abril", "nati"]
+
+# array.push(elemento) / agregar elementos
+personas.append("pablo")
+print(personas)
+
+#para eliminar a elementos en array
+personas.pop(0) #elimino a ore y no al ultimo por eso le tengo que poner la posición 
+print(personas)
+
+personas.remove("abril") #aqui elmino por parámetros no por posición como con el .POP
+print(personas)
+
+#para imprimir de una lista de array unos cuantos solo puedo hacer lo siguietne: 
+print(personas[1:3]) #me imprime abril y nati
+print(personas[:-2]) #me imprime ore, es decir desde el comienzo menos la ultima persona(si pongo -1 me incluye la ultima persona xk esta es siempre n-1)
+print(len("hola que tal"))#"para medir la longitud de un string"
+print(len(personas)) #para medir objetos u array
+
+
+
+
+
+
+
 
