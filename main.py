89667afe201bbbcbb5cbb5a444eda0c755f70784
natigalsan <natividadgalsan@gmail.com>
2022-05-13 17:30:30 +0000

    python

---
+++ main.py
@@ -0,0 +1,24 @@
+nombre = "Macarena"
+edad = 15
+lenguajes_fav = ['Python', 'C', 'JS']
+
+
+print ("hello geeks "+ nombre + "tengo", edad, "Y mis lenguajes favoritos son:", lenguajes_fav)
+print (f" hola mi nombre es {nombre} tengo {edad} y mis leng favor son {lenguajes_fav}")
+
+persona = {
+    "nombre": "Macarena",
+    "edad": 26,
+    "leg fav": ['Python', 'C', 'JS']
+}
+print(persona["leg fav"])
+
+# Sentencia condicional:
+
+if (edad >17):
+    print ("hola")
+elif(edad==15):
+    print("adios, no puedes beber así que para casita!!!") 
+else: 
+    print("no deberías pensar en eso")
+
